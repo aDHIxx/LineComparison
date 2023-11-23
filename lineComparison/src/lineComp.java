79b07d667c6c8bd99194b487b52a38c2d1042c50
aDHIxx <sreemohan.adhithya@gmail.com>
2023-11-23 16:54:37 +0530

    Added: UseCase 2: Check two lines are equal or not using equals

--- lineComparison/src/lineComp.java
+++ lineComparison/src/lineComp.java
@@ -9,31 +9,43 @@ public class lineComp {
         this.y2=y2;
     }
     /**
-     * @name: calculateLength
-     * @return/ o/p: length of line using the distance formula.
+     * @name: equals
+     * @return/ o/p: true if the lines are equal, false otherwise
      */
-    public double calculateLength() {
-        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+    public boolean equals(lineComp other) {
+        return this.x1 == other.x1 && this.y1 == other.y1 && this.x2 == other.x2 && this.y2 == other.y2;
     }
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Line Comparison Computation Program");
-
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("x1 y1 : ");
-        double x1 = scanner.nextDouble();
-        double y1 = scanner.nextDouble();
+        System.out.print("Line 1: x1  y1: ");
+        double x1Line1 = scanner.nextDouble();
+        double y1Line1 = scanner.nextDouble();
 
-        System.out.print("x2 y2 : ");
-        double x2 = scanner.nextDouble();
-        double y2 = scanner.nextDouble();
+        System.out.print("Line 1: x2  y2: ");
+        double x2Line1 = scanner.nextDouble();
+        double y2Line1 = scanner.nextDouble();
 
-        lineComp line = new lineComp(x1, y1, x2, y2);
+        lineComp line1 = new lineComp(x1Line1, y1Line1, x2Line1, y2Line1);
 
-        double length = line.calculateLength();
-        System.out.println("Length of the line: " + length);
+        System.out.print("Line 2: x1  y1: ");
+        double x1Line2 = scanner.nextDouble();
+        double y1Line2 = scanner.nextDouble();
+
+        System.out.print("Line 2: x2  y2: ");
+        double x2Line2 = scanner.nextDouble();
+        double y2Line2 = scanner.nextDouble();
+
+        lineComp line2 = new lineComp(x1Line2, y1Line2, x2Line2, y2Line2);
+
+        if (line1.equals(line2)) {
+            System.out.println("Lines are equal.");
+        } else {
+            System.out.println("Lines are not equal.");
+        }
 
         scanner.close();
     }
