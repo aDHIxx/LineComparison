4a5fa3ba5d9b43db4ff7cce914e1e71697ceb0ab
aDHIxx <sreemohan.adhithya@gmail.com>
2023-11-27 18:19:44 +0530

    Added: Usecase 4: Created class Line and Point and then used equals() and compareTo()

--- lineComparison/src/lineComp.java
+++ lineComparison/src/lineComp.java
@@ -1,5 +1,85 @@
 import java.util.Scanner;
 
+/*
+ * @name: Point
+ * @i/p: x and y coordinates
+ * @return/ o/p: Point object with x and y coordinates
+ */
+class Point {
+    private double x, y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // getters for x and y
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    // Override equals method for Point
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Point point = (Point) obj;
+        return Double.compare(point.x, x) == 0 && Double.compare(point.y, y) == 0;
+    }
+}
+/*
+ * @name: Line
+ * @i/p: startPoint and endPoint
+ * @return/ o/p: Line object with startPoint and endPoint
+ */
+class Line {
+    private Point startPoint, endPoint;
+
+    public Line(Point startPoint, Point endPoint) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+    }
+
+    // Getter methods for startPoint and endPoint
+
+    public Point getStartPoint() {
+        return startPoint;
+    }
+
+    public Point getEndPoint() {
+        return endPoint;
+    }
+
+    // Method to calculate the length of the line
+    public double calculateLength() {
+        return Math.sqrt(Math.pow(endPoint.getX() - startPoint.getX(), 2) +
+                Math.pow(endPoint.getY() - startPoint.getY(), 2));
+    }
+
+    // Override equals method for Line
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Line line = (Line) obj;
+        return startPoint.equals(line.startPoint) && endPoint.equals(line.endPoint);
+    }
+    /*
+     * @name: compareTo
+     * @i/p: otherLine - the other line
+     * @return/ o/p: 0 if  equal, +ve no if line1 is greater, -ve no if line2 is greater.
+     */
+    public int compareTo(Line otherLine) {
+        double thisLength = calculateLength();
+        double otherLineLength = otherLine.calculateLength();
+        return Double.compare(thisLength, otherLineLength);
+    }
+}
 public class lineComp {
     private double x1, y1, x2, y2;
     public lineComp(double x1, double y1, double x2, double y2) {
@@ -8,7 +88,7 @@ public class lineComp {
         this.x2=x2;
         this.y2=y2;
     }
-    /**
+    /*
      * @name: calculateLength
      * @return/ o/p: Length of the line.
      */
@@ -16,7 +96,7 @@ public class lineComp {
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 
-    /**
+    /*
      * @name: compareTo
      * @i/p: other - the other line
      * @return/ o/p: 0 if  equal, +ve no if line1 is greater, -ve no if line2 is greater.
@@ -32,25 +112,13 @@ public class lineComp {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Line 1: x1  y1: ");
-        double x1Line1 = scanner.nextDouble();
-        double y1Line1 = scanner.nextDouble();
+        Point startPoint1 = readPoint(scanner, "Line 1: Starting point (x y): ");
+        Point endPoint1 = readPoint(scanner, "Line 1: Ending point (x y): ");
+        Line line1 = new Line(startPoint1, endPoint1);
 
-        System.out.print("Line 1: x2  y2: ");
-        double x2Line1 = scanner.nextDouble();
-        double y2Line1 = scanner.nextDouble();
-
-        lineComp line1 = new lineComp(x1Line1, y1Line1, x2Line1, y2Line1);
-
-        System.out.print("Line 2: x1  y1: ");
-        double x1Line2 = scanner.nextDouble();
-        double y1Line2 = scanner.nextDouble();
-
-        System.out.print("Line 2: x2  y2: ");
-        double x2Line2 = scanner.nextDouble();
-        double y2Line2 = scanner.nextDouble();
-
-        lineComp line2 = new lineComp(x1Line2, y1Line2, x2Line2, y2Line2);
+        Point startPoint2 = readPoint(scanner, "Line 2: Starting point (x y): ");
+        Point endPoint2 = readPoint(scanner, "Line 2: Ending point (x y): ");
+        Line line2 = new Line(startPoint2, endPoint2);
 
         int result = line1.compareTo(line2);
         if (result == 0) {
@@ -62,5 +130,18 @@ public class lineComp {
         }
 
         scanner.close();
+    }
+
+    /*
+     * @name: readPoint
+     * @i/p: scanner - Scanner object to read input from console
+     * prompt - prompt to be displayed to user
+     * @return/ o/p: Point object with x and y coordinates
+     */
+    private static Point readPoint(Scanner scanner, String prompt) {
+        System.out.print(prompt);
+        double x = scanner.nextDouble();
+        double y = scanner.nextDouble();
+        return new Point(x, y);
     }
 }
